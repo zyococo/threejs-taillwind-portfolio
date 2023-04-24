3426746f8d9db66381cda46bbe33bfb17603b19c
g1348032 <g1348032@icloud.com>
2023-04-24 23:55:30 +0900

    Vertical line  modified

--- src/components/SphereGeometry.jsx
+++ src/components/SphereGeometry.jsx
@@ -9,6 +9,7 @@ const SphereGeometry = () => {
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
+        {/* green vertical line */}
         {/* <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#68fd46]" />
           <div className="w-1 sm:h-80 h-40 bg-gradient-to-b from-green-300" />
